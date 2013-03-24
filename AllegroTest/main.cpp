43fb872ee562ebe55fcfc6b43692ec4a54d88d79
brandontom <brandontom@live.com>
2013-03-24 16:46:13 -0400

    fdslf

--- AllegroTest/main.cpp
+++ AllegroTest/main.cpp
@@ -1,112 +1,45 @@
-#include <stdio.h>
-#include <allegro5/allegro.h>
- 
-const float FPS = 60;
-const int SCREEN_W = 640;
-const int SCREEN_H = 480;
-const int BOUNCER_SIZE = 32;
- 
-int main(int argc, char **argv){
-   ALLEGRO_DISPLAY *display = NULL;
-   ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-   ALLEGRO_TIMER *timer = NULL;
-   ALLEGRO_BITMAP *bouncer = NULL;
-   float bouncer_x = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
-   float bouncer_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
-   float bouncer_dx = -4.0, bouncer_dy = 4.0;
-   bool redraw = true;
- 
-   if(!al_init()) {
-      fprintf(stderr, "failed to initialize allegro!\n");
-      return -1;
-   }
- 
-   //brandon nolan tom
+#include <allegro5\allegro.h>
+#include <allegro5\allegro_font.h>
+#include <allegro5\allegro_ttf.h>
+#include <allegro5\allegro_native_dialog.h>
 
-   timer = al_create_timer(1.0 / FPS);
-   if(!timer) {
-      fprintf(stderr, "failed to create timer!\n");
-      return -1;
-   }
- 
-   display = al_create_display(SCREEN_W, SCREEN_H);
-   if(!display) {
-      fprintf(stderr, "failed to create display!\n");
-      al_destroy_timer(timer);
-      return -1;
-   }
- 
-   bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
-   if(!bouncer) {
-      fprintf(stderr, "failed to create bouncer bitmap!\n");
-      al_destroy_display(display);
-      al_destroy_timer(timer);
-      return -1;
-   }
- 
-   al_set_target_bitmap(bouncer);
- 
-   al_clear_to_color(al_map_rgb(255, 0, 255));
- 
-   al_set_target_bitmap(al_get_backbuffer(display));
- 
-   event_queue = al_create_event_queue();
-   if(!event_queue) {
-      fprintf(stderr, "failed to create event_queue!\n");
-      al_destroy_bitmap(bouncer);
-      al_destroy_display(display);
-      al_destroy_timer(timer);
-      return -1;
-   }
- 
-   al_register_event_source(event_queue, al_get_display_event_source(display));
- 
-   al_register_event_source(event_queue, al_get_timer_event_source(timer));
- 
-   al_clear_to_color(al_map_rgb(0,0,0));
- 
-   al_flip_display();
- 
-   al_start_timer(timer);
- 
-   while(1)
-   {
-      ALLEGRO_EVENT ev;
-      al_wait_for_event(event_queue, &ev);
- 
-      if(ev.type == ALLEGRO_EVENT_TIMER) {
-         if(bouncer_x < 0 || bouncer_x > SCREEN_W - BOUNCER_SIZE) {
-            bouncer_dx = -bouncer_dx;
-         }
- 
-         if(bouncer_y < 0 || bouncer_y > SCREEN_H - BOUNCER_SIZE) {
-            bouncer_dy = -bouncer_dy;
-         }
- 
-         bouncer_x += bouncer_dx;
-         bouncer_y += bouncer_dy;
- 
-         redraw = true;
-      }
-      else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-         break;
-      }
- 
-      if(redraw && al_is_event_queue_empty(event_queue)) {
-         redraw = false;
- 
-         al_clear_to_color(al_map_rgb(0,0,0));
- 
-         al_draw_bitmap(bouncer, bouncer_x, bouncer_y, 0);
- 
-         al_flip_display();
-      }
-   }
- 
-   al_destroy_bitmap(bouncer);
-   al_destroy_timer(timer);
-   al_destroy_display(display);
-   al_destroy_event_queue(event_queue);
- 
-   return 0;
+int main (void)
+{
+
+ALLEGRO_DISPLAY *display = NULL;
+
+if(!al_init())
+{
+    al_show_native_message_box(NULL, NULL, NULL,
+        "failed to initialize allegro!", NULL, NULL);
+    return -1;
+}
+
+display = al_create_display(640, 480);
+
+if(!display)
+{
+    al_show_native_message_box(NULL, NULL, NULL,
+        "failed to initialize display!", NULL, NULL);
+    return -1;
+}
+
+al_init_font_addon();
+al_init_ttf_addon();
+
+ALLEGRO_FONT *font24 = al_load_font("arial.ttf", 24, 0);
+ALLEGRO_FONT *font36 = al_load_font("arial.ttf", 36, 0);
+ALLEGRO_FONT *font18 = al_load_font("arial.ttf", 18, 0);
+
+al_clear_to_color(al_map_rgb(0,0,0));
+
+al_draw_text(font24, al_map_rgb(255, 0, 255), 50, 50, 0, "Hello World, this is 24 point");
+
+al_flip_display();
+
+al_rest(5.0);
+
+al_destroy_display(display);
+return 0;
+
 }
